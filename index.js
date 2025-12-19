9cae04d79780858f58da03229161fd0d487eba66
zoooz555 <zessam227@gmail.com>
2025-12-19 21:16:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+//const path =require('path');
+ // const fs =require('fs');
+
+
+  //function logCurrentPath(){
+  //  console.log({File: __filename,Dir:  dirname});
+  //}
+
+  //function getFileName(filePath){
+    //return Path.basename(filePath);
+  //}
+
+
+
+  //function buildPathFromObject(pathObj){
+    //return path.format(pathObj);
+  //}
+
+
+
+  //function getExtension(filePath){
+   // return path.extname(filePath);
+  //}
+
+
+
+
+ // function parsePath(filePath){
+   // const parsed =path.parse(filePath);
+    //return {Name: parsed.name, Ext:parsed.Ext};
+  //}
+
+
+
+
+ // function isAbsolutePath(filePath){
+   // return path.isAbsolute(filePath);
+  //}
+
+
+
+
+  //function JoinSegments(segments){
+    //return path.join(segments);
+  //}
+
+
+
+  //function resolveToAbsolute(relativePath){
+   // return path.resolve(relativePath);
+  //}
+
+
+
+  //function JoinTwoPaths(path1, path2){
+    //return path.join(path1,path2);
+  //}
+
+
+
+  //function delteFileAsyns(filePath){
+    //fs.unlink(filePath,(err)=>{
+      //  if (err) throw err;
+    //console.log('The{path.basename(filePath)} is deleted');
+        
+   // });
+    
+  //}
+
+
+
+  //function createFolderSync(folderName){
+    //try{
+      //  if (!fs.existSync(folderName)){
+        //    fs.mkdirSync(folderName);
+          //  return "Success";
+        //}
+    //} catch (err){
+      //  return "Error"+err.message;
+    //}
+ // }
+
+//const myEmitter =new EventEmitter();
+ 
+ //myEmitter.on('start',() => {
+   // console.log("welcome event triggered");
+
+ //});
+
+
+
+
+// myEmitter.on('login',(Ahmed) =>{
+  //  console.log('User Logged in:{Ahmed}');
+ //});
+
+
+
+// function readFileSyncAndLog(path){
+  //  try{
+  //const content =fs.readFileSync(path,'utf8');
+  //console.log('the file content => "./notes.txt"');
+    //}catch(err){
+      //  console.error("Error reading file:",err.message);
+    //}
+ //}
+
+
+
+
+ //function writeFileAsync(path,content){
+   // fs.writeFile(path,content,(err) =>{
+     //   if (err)throw err;
+       // console.log("File has been saved asynchronously!");
+    //});
+ //}
+
+
+
+// function checkDirectoryExists(path){
+  //  const exists =fs.existsSync(path);
+    //console.log(exists);
+    //return exists;
+ //}
+
+
+ 
+    
+ 
